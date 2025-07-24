9f6c9146bbaa2f8299bcb4336a7941b576bf3145
Habib Opeyemi Adebayo <adebayohabib7@gmail.com>
2025-07-24 14:20:22 +0100

    Update emailService.ts

--- server/src/services/emailService.ts
+++ server/src/services/emailService.ts
@@ -12,8 +12,8 @@ class EmailService {
     this.transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
     },
   });
  }
